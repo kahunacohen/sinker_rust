2636540d9169cc35b4dcf949186354a68ac24fa6
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-03-21 10:13:49 +0200

    add log

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,25 @@ use clap::{load_yaml, App};
 use std::fmt;
 
 struct Config {
-    pub verbose: bool,
+    pub log: bool,
 }
 impl fmt::Display for Config {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Config{{display: {}}}", self.verbose)
+        write!(f, "Config{{log: {}}}", self.log)
+    }
+}
+impl Config {
+    fn new(matches: &clap::ArgMatches) -> Config {
+        Config {
+            log: matches.is_present("log"),
+        }
     }
 }
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
-    let conf = Config {
-        verbose: matches.is_present("verbose"),
-    };
+    let conf = Config::new(&matches);
+
     print!("{}", conf)
 }
