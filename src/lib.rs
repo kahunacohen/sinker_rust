1d512a7eb5e38d3b825ea70a09783dde85b9a1c5
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-03-22 10:32:58 +0200

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
+use log::info;
 use std::fmt;
+use std::fs;
+use std::io::Error;
+use std::io::Result as IoResult;
+use std::result::Result;
 
+pub struct GistFile {
+  path: String,
+  id: String,
+}
+pub struct Gist {
+  access_token: String,
+  files: Vec<GistFile>,
+}
 pub struct Config {
   pub log: bool,
+  pub s: Option<String>, //pub gist: Option<Gist>,
 }
 impl fmt::Display for Config {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -9,14 +23,30 @@ impl fmt::Display for Config {
   }
 }
 impl Config {
-  pub fn new(matches: &clap::ArgMatches) -> Config {
-    Config {
+  pub fn new(matches: &clap::ArgMatches) -> Result<Config, Error> {
+    let c = Config {
       log: matches.is_present("log"),
+      s: None, //gist: None,
+    };
+    match c.read_config_file() {
+      Ok(s) => {
+        c.s = Some(s);
+        Ok(c)
+      }
+      Err(why) => Err(why),
     }
+  }
+  pub fn read_config_file(&self) -> IoResult<String> {
+    if self.log {
+      info!("reading config file");
+    }
+    return fs::read_to_string("/Users/acohen/.sinkerrc.json");
   }
 }
 
 pub fn run(matches: clap::ArgMatches) {
-  let conf = Config::new(&matches);
-  print!("{}", conf)
+  match Config::new(&matches) {
+    Ok(c) => print!("{}", c),
+    Err(why) => println!("{}", why),
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use sinker::run;
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
-    let matches = App::from(yaml).get_matches();
-    run(matches);
+    let cli = App::from(yaml).get_matches();
+    run(cli);
 }
