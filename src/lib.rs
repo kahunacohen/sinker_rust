8de0c2d7f3427f04f3827d6b1186eb4f3f3a7bb1
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-03-25 14:16:33 +0200

    config runs

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,60 @@
-use log::info;
-use std::fmt;
-use std::fs;
-use std::io::Result as IoResult;
-use std::io::{Error, ErrorKind};
+// use log::info;
+use std::error::Error;
+// use std::fmt;
+// use std::fs;
 use std::result::Result;
 
+use std::fs::File;
+
+use std::io::BufReader;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 pub struct GistFile {
   path: String,
   id: String,
 }
+
+#[derive(Deserialize)]
 pub struct Gist {
   access_token: String,
   files: Vec<GistFile>,
 }
+
 pub struct Config {
   pub log: bool,
-  pub s: IoResult<String>, //pub gist: Option<Gist>,
+  pub gist: Result<Gist, Box<dyn Error>>,
 }
 struct CustomError(String);
 
-impl fmt::Display for Config {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "Config{{log: {}, s: {:?}}}", self.log, self.s)
-  }
+// impl fmt::Display for Config {
+//   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//     write!(f, "Config{{log: {}, s: {:?}}}", self.log, self.s)
+//   }
+// }
+fn parse_config_file() -> Result<Gist, Box<dyn Error>> {
+  // Open the file in read-only mode with buffer.
+  // If opening fails, return Result with Error varient.
+  let file = File::open("/Users/acohen/.sinkerrc.json")?;
+  let reader = BufReader::new(file);
+  // Read the JSON contents of the file as an instance of `Gist`. Again, if error, return
+  //early and propogate to caller.
+  let gist = serde_json::from_reader(reader)?;
+  return Ok(gist);
 }
 impl Config {
   pub fn new(matches: &clap::ArgMatches) -> Config {
     return Config {
       log: matches.is_present("log"),
-      s: fs::read_to_string("/Users/acohen/.sinkerrc.json").map_err(|err| {
-        Error::new(
-          ErrorKind::InvalidInput,
-          format!("not able to read config file: {}", err),
-        )
-      }), //gist: None,
+      gist: parse_config_file(),
     };
   }
 }
 
 pub fn run(matches: clap::ArgMatches) {
-  let c = Config::new(&matches);
-  println!("{}", c);
+  Config::new(&matches);
+  println!("Ran!");
 }
 
 fn add(x: u8, y: u8) -> u8 {
